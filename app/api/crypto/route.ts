3aa612a0a687a1dcf00e76bf6f4010058e3ba6a8
asarama <arshameslami@hotmail.com>
2023-11-20 03:16:51 -0500

    Added initial version of cryto endpoint

---
+++ app/api/crypto/route.ts
@@ -0,0 +1,30 @@
+import {NextResponse} from 'next/server'
+
+export async function GET(req: Request) {
+    console.log(req.url)
+    const { search } = new URL(req.url)
+
+    const baseEndpoint = "https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+    const endpoint = `${baseEndpoint}${search}`
+
+    // TODO: Move to .env file
+    const apiKey = "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c"
+    const headers = { 
+        'Content-Type': 'application/json',
+        'X-CMC_PRO_API_KEY': apiKey
+    }
+
+    const data = await basicFetch(endpoint, headers)
+
+    return NextResponse.json(data)
+}
+
+const basicFetch = async <ReturnType>(endpoint: string, headers: any): Promise<ReturnType> => {
+    const response = await fetch(endpoint, {headers: headers})
+
+    if (!response.ok) throw new Error('Error!')
+
+    const data = await response.json();
+
+    return data
+}
