c37ac38fba0c5e7795af7255c169a2d5d02a6ce4
asarama <arshameslami@hotmail.com>
2023-11-20 02:32:29 -0500

    Install and test mobx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 
+if (!new class { x }().hasOwnProperty('x')) throw new Error('Transpiler is not configured correctly');
+
 export default function HomePage() {
   return (
     <>
